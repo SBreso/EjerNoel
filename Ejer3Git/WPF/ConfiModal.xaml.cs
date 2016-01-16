451d918512315321a05e6b99155d836c94884124
Salvador <sbresograncha@gmail.com>
2016-01-16 14:31:26 +0100

    Logica de la ventana modal. parte 1

---
+++ Ejer3Git/WPF/ConfiModal.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ejer3Git
+{
+    /// <summary>
+    /// Lógica de interacción para ConfiModal.xaml
+    /// </summary>
+    public partial class ConfiModal : Window
+    {
+        public List<ColorNavidad> xtmasColorList;
+        public Color colorAns;
+        public Boolean ballVisivility;
+        public ConfiModal()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Añadimos elementos a la lista
+        /// </summary>
+        private void addColorNavidad()
+        {
+            xtmasColorList = new List<ColorNavidad>(){
+                new ColorNavidad(Colors.Red, "Rojo","Navidades calurosas"),
+            new ColorNavidad(Colors.Olive, "Olive","Navidades mediterraneas"),
+            new ColorNavidad(Colors.Peru,"Peru","Navidades finlandesas"),
+            new ColorNavidad(Colors.DarkCyan,"Cyan","Navidad cercana al mar")
+            };
+        }
+        /// <summary>
+        /// Cada vez que cargamos la ventanaModal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.addColorNavidad();
+            this.changeColorBall.ItemsSource = xtmasColorList;
+            this.changeColorBall.SelectedItem = xtmasColorList.ElementAt(0);
+        }
+        /// <summary>
+        /// Aceptamos
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            this.colorAns = ((ColorNavidad)this.changeColorBall.SelectedItem).Color;
+            this.ballVisivility = (bool)showBalls.IsChecked;
+            base.Close();
+        }
+        /// <summary>
+        /// Cancelamos
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            base.Close();
+        }
+    }
+    /// <summary>
+    /// Clase que representa los elementos que introduciremos en el ComboBox
+    /// </summary>
+    public class ColorNavidad
+    {
+        public Color Color{get;set;}
+        public String Nombre{get;set;}
+        public String Estado{get;set;}
+        public ColorNavidad(Color c, String n, String e)
+        {
+            this.Color=c;
+            this.Nombre=n;
+            this.Estado=e;
+        }
+    }
+}
---
+++ Ejer3Git/WPF/Postal.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejer3Git
+{
+    /// <summary>
+    /// Lógica de interacción para Postal.xaml
+    /// </summary>
+    public partial class Postal : Window
+    {
+        StarsNoModal StarsWin;
+        public Postal()
+        {
+            InitializeComponent();
+            base.Closing += new System.ComponentModel.CancelEventHandler(Exit);
+            this.StarsWin = new StarsNoModal();
+        }
+        /// <summary>
+        /// Manejador del evento que se produce al cerra la app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Exit(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!this.StarsWin.IsActive)
+            {
+                this.StarsWin.Close();
+            }
+            base.Visibility = Visibility.Hidden;
+            MessageBox.Show("Y FELIZ NAVIAD!!");
+        }
+        /// <summary>
+        /// Al presionar en 'OK' salimos de la aplicacion
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            base.Close();
+        }
+
+        private void animacionButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.StarsWin.Show();
+        }
+
+        private void confiButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConfiModal confiWin = new ConfiModal();
+            confiWin.Owner = this;
+            confiWin.ShowDialog();
+            if (confiWin.DialogResult == true)
+            {
+                
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
