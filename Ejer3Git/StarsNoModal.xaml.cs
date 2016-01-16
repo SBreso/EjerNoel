156000c1a6a38c1b65200b6097b9bd2879e5ecb4
Salvador <sbresograncha@gmail.com>
2016-01-16 13:52:31 +0100

    Diseñada la ventana de strellas

---
+++ Ejer3Git/StarsNoModal.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ejer3Git
+{
+    /// <summary>
+    /// Lógica de interacción para StarsNoModal.xaml
+    /// </summary>
+    public partial class StarsNoModal : Window
+    {
+        public StarsNoModal()
+        {
+            InitializeComponent();
+        }
+    }
+}
