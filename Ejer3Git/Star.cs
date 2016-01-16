52528c70dd27ead83c8db79a3ca8b347cb09c570
Salvador <sbresograncha@gmail.com>
2016-01-16 17:09:49 +0100

    Mostramos las estrellas

---
+++ Ejer3Git/Star.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Ejer3Git
+{
+    class Star
+    {
+        //contenido del label
+        char content = '*';
+        //posicion en el canvas
+        int x;
+        int y;
+        //variacion de opacidad
+        int vari;
+        int difOp;
+        Label labelStar;
+        Grid panel;
+        static Random rnd = new Random();
+        public Star(Grid g)
+        {
+            this.panel = g;
+            //valores para todos los labels
+            this.labelStar = new Label();
+            this.labelStar.Content = this.content;
+            this.labelStar.VerticalAlignment = VerticalAlignment.Bottom;
+            this.labelStar.Foreground = new SolidColorBrush(Colors.White);
+            this.labelStar.SetValue(Panel.ZIndexProperty,-2);
+            this.labelStar.FontSize = Star.Rnd(10, 20);
+            //para cada label es diferente
+            LabelConfiguration();
+            //lo añadimos al grid
+            this.panel.Children.Add(this.labelStar);
+            //lo mostramos
+            this.PrintLabel();
+        }
+        /// <summary>
+        /// Configuracion del labelStar
+        /// </summary>
+        private void LabelConfiguration()
+        {
+            //posicion aleatoria dentro del gridCenter
+            this.x = Star.Rnd(0, (int)panel.ActualWidth);
+            this.y = Star.Rnd(118, (int)panel.ActualHeight);//el 118 es la altura del primer arbol, asi las estrellas quedan por encima suyo
+            //variables para cambiar la opacidad
+            this.vari = Star.Rnd(1,10);
+            this.difOp = 1;
+        }
+        /// <summary>
+        /// Dibujar el label
+        /// </summary>
+        public void PrintLabel()
+        {
+            this.labelStar.Visibility = Visibility.Visible;
+            this.labelStar.Margin = new Thickness(this.x,0,0,this.y);
+            this.difOp += this.vari;
+            //la opacidad va a %
+            this.labelStar.Foreground.Opacity =(double)(1 / (float)this.difOp);
+            //cuando llegue un momento que sea tan transparente que no se vea, cambiamos
+            //su posicion
+            if (this.difOp > 50)
+            {
+                this.LabelConfiguration();
+            }
+        }
+        /// <summary>
+        /// Ocultar label
+        /// </summary>
+        public void HideLabel()
+        {
+            this.labelStar.Visibility = Visibility.Hidden;
+        }
+        //obtener un valor aleatorio entre a y b
+        private static int Rnd(int a, int b)
+        {
+            try { return Star.rnd.Next(a,b);}
+            catch (Exception ex) { return 0; }
+        }
+        
+
+    }
+}
--- Ejer3Git/WPF/Postal.xaml.cs
+++ Ejer3Git/WPF/Postal.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Ejer3Git
 {
@@ -20,12 +21,33 @@ namespace Ejer3Git
     /// </summary>
     public partial class Postal : Window
     {
+        //ventana no modal
         StarsNoModal StarsWin;
+        //Lista de star
+        List<Star> starList;
+        //num de estrellas
+        int countStars;
+        //color de las bolas
+        Color colorBall;
+        //visibilidad de las bolas
+        Boolean ballVisivility;
+        //para controlar el movimiento del label y las estrellas
+        DispatcherTimer timer = new DispatcherTimer();
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public Postal()
         {
             InitializeComponent();
             base.Closing += new System.ComponentModel.CancelEventHandler(Exit);
+            //ventana no modal
             this.StarsWin = new StarsNoModal();
+            //numero de estrellas de inicio
+            this.countStars = 20;
+            //color de inicio de bolas
+            this.colorBall = Colors.Red;
+            //de inicio las bolas se ven
+            this.ballVisivility = true;
         }
         /// <summary>
         /// Manejador del evento que se produce al cerra la app
@@ -42,7 +64,7 @@ namespace Ejer3Git
             MessageBox.Show("Y FELIZ NAVIAD!!");
         }
         /// <summary>
-        /// Al presionar en 'OK' salimos de la aplicacion
+        /// al presionar en 'OK' salimos de la aplicacion
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -50,12 +72,20 @@ namespace Ejer3Git
         {
             base.Close();
         }
-
+        /// <summary>
+        /// Mostrar la ventana para escoger el numero de estrellas
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void animacionButton_Click(object sender, RoutedEventArgs e)
         {
             this.StarsWin.Show();
         }
-
+        /// <summary>
+        /// Mostrar la ventana de configuracion
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void confiButton_Click(object sender, RoutedEventArgs e)
         {
             ConfiModal confiWin = new ConfiModal();
@@ -68,6 +98,50 @@ namespace Ejer3Git
             else
             {
 
+            }
+        }
+        /// <summary>
+        /// Cargar la ventana
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.starList = new List<Star>();
+            for (int i = 0; i < 40; i++)
+            {
+                this.starList.Add(new Star(this.gridCenter));
+            }
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+        /// <summary>
+        /// Que hacer cada tick. Mover el labelNoel. Mostrar estrellas
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (this.labelNoel.ActualWidth + this.labelNoel.Margin.Left <= 0)
+            {
+                this.labelNoel.Margin = new Thickness(base.Width, 0, 0, 0);
+            }
+            else
+            {
+                this.labelNoel.Margin = new Thickness(this.labelNoel.Margin.Left-15,0,0,0);
+            }
+            //Tengo todos los labels, solo que segun lo que diga el usuario muestro unos u otros
+            for (int i = 0; i < 40; i++)
+            {
+                if (i < this.countStars)
+                {
+                    ((Star)this.starList.ElementAt(i)).PrintLabel();
+                }
+                else
+                {
+                    ((Star)this.starList.ElementAt(i)).HideLabel();
+                }
             }
         }
     }
